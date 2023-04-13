e9eaf9078b772bd480660253c9587bd37d5b39fc
ConorPadraigMurphy <conorpadraigmurphy@gmail.com>
2023-04-13 18:07:35 +0100

    Spawner can now spawn an array of enemies

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -4,55 +4,33 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    //[SerializeField] GameObject enemyPrefab;
-    //public float interval = 0;
-    private float counter = 0;
     private int enemyamount;
     bool isSpawning = true;
-    [SerializeField] private int maxEnemies = 0;
+    [SerializeField] private int maxEnemies = 1;
     [SerializeField] private float spawnRate = 2f;
     [SerializeField] GameObject[] enemyGroupPrefabs;
 
-    // void Update()
-    // {
-    //     counter += Time.deltaTime;
-    //     if (counter >= interval)
-    //     {
-    //         if (enemyamount < maxEnemies)
-    //         {
-
-    //             counter = 0;
-    //         }
-    //     }
-    // }
-
-    void Update()
+    void Start()
     {
-
         StartCoroutine(SpawnEnemies());
-        // if (enemyamount >= maxEnemies)
-        // {
-        //     isSpawning = false;
-        // }
-
     }
 
     IEnumerator SpawnEnemies()
     {
         WaitForSeconds wait = new WaitForSeconds(spawnRate);
+
         while (isSpawning == true)
         {
             yield return wait;
             int randomenemy = Random.Range(0, enemyGroupPrefabs.Length);
             GameObject randomEnemyGroup = enemyGroupPrefabs[randomenemy];
-            GameObject Enemyclone = Instantiate(randomEnemyGroup, transform.position, transform.rotation);
+            Instantiate(randomEnemyGroup, transform.position, transform.rotation);
             enemyamount++;
             Debug.Log("Enemy amount: " + enemyamount);
+            if (enemyamount >= maxEnemies)
+            {
+                isSpawning = false;
+            }
         }
-    }
-
-    public void SpawnEnemy()
-    {
-
     }
 }
